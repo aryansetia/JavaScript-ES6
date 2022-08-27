77f4a4a44046059969194f66d0b9cf7d35fe8b87
Aryan Setia <aryansetiaa@gmail.com>
2022-08-27 22:24:26 +0530

    spread operator and for of loop

--- for_of_loop.js
+++ for_of_loop.js
@@ -1,11 +1,20 @@
-let incomes = [62000, 75000, 67500]
-let total = 0;
+// let incomes = [62000, 75000, 67500]
+// let total = 0;
 
-for (const income of incomes) {
-    total += income;
+// for (const income of incomes) {
+//     total += income;
+// }
+
+// let text = 'Hello World'
+// for (const i of text) {
+//     console.log(i);
+// }
+
+// practice challenge
+let incomes = [62000, 75000, 67500];
+
+for (let income of incomes) {
+    income += 5000;
 }
 
-let text = 'Hello World'
-for (const i of text) {
-    console.log(i);
-}
+console.log(incomes);
