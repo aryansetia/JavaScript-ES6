c54405a1d61bd49c29b765d9eee43367cca5032e
Aryan Setia <aryansetiaa@gmail.com>
2022-08-27 21:42:28 +0530

    for of loop

---
+++ for_of_loop.js
@@ -0,0 +1,11 @@
+let incomes = [62000, 75000, 67500]
+let total = 0;
+
+for (const income of incomes) {
+    total += income;
+}
+
+let text = 'Hello World'
+for (const i of text) {
+    console.log(i);
+}
