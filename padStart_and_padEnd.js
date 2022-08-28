8e7e97bce2e7fc2b19ce37684624340512bc3907
Aryan Setia <aryansetiaa@gmail.com>
2022-08-28 11:56:31 +0530

    padStart() and padEnd()

--- padStart_and_padEnd.js
+++ padStart_and_padEnd.js
@@ -1,4 +1,8 @@
 let example = 'England';
 
-console.log(example.padStart(10, 'E'));
-console.log(example.padEnd(10,'E'));
+// console.log(example.padStart(10, 'E'));
+// console.log(example.padEnd(10,'E'));
+
+//practice challenge 
+console.log(example.padStart(100));
+console.log(example.padEnd(100).length)
